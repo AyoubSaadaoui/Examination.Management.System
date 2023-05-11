882031705e39644f56eaf0294306d3df41d73b14
Ayoub Saadaoui <saadaouiayoub.08@gmail.com>
2023-05-11 15:00:33 +0100

    load our css which is a bootstrap and fontawesomme

--- private/core/controller.php
+++ private/core/controller.php
@@ -9,10 +9,10 @@ class Controller {
 
         extract($data);
         if(file_exists("../private/views/" . $view . ".view.php")) {
-            return file_get_contents("../private/views/" . $view . ".view.php");
+            require ("../private/views/" . $view . ".view.php");
         }
         else {
-            return file_get_contents("../private/views/404.view.php");
+            require ("../private/views/404.view.php");
         }
     }
 }
--- private/views/home.view.php
+++ private/views/home.view.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/all.min.css">
 </head>
 <body>
-    <h1>This is home</h1>
+    <div class="container-fluid ">
+        <h1><i class="fa fa-plus"></i> This is home</h1>
+    </div>
 </body>
 </html>
