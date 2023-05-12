af8048f0f4a4f5e7b08a7d8c835204bcb99811e0
Ayoub Saadaoui <saadaouiayoub.08@gmail.com>
2023-05-13 00:34:16 +0100

    Create  functions for CRUD

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -8,8 +8,21 @@ class Home extends Controller
     function index() {
 
         $user = new User();
-        $data = $user->findAll();
+
         // $data = $user->where('firstname', 'Ayoub');
+
+        // $arr['firstname'] = 'Mohammed Amine';
+        // $arr['lastname'] = 'Alami';
+        // $arr['date'] = '2023-05-12 17:24:01';
+        // $arr['user_id'] = 'sdjhbcjd';
+        // $arr['gender'] = 'female';
+        // $arr['school_id'] = 'bbhjhbjh';
+        // $arr['rank'] = 'student';
+
+        // $user->insert($arr);
+        // $user->update(3,$arr);
+        // $user->delete(2);
+        $data = $user->findAll();
 
         $this->view('home', ['rows'=>$data]);
     }
--- private/core/model.php
+++ private/core/model.php
@@ -25,4 +25,37 @@ class Model extends Database {
         return $this->query($query);
     }
 
+    public function insert($data) {
+
+        $keys = array_keys($data);
+        $columns = implode(',', $keys);
+        $values = implode(',:', $keys);
+
+        $query = "INSERT INTO $this->table ($columns) VALUES (:$values)";
+
+        return $this->query($query, $data);
+    }
+
+    public function update($id, $data) {
+
+        $str = "";
+        foreach($data as $key => $value){
+            // ".=" will concatenate strings
+            // value1 =: value1 , ...
+            $str .= $key . "=:" . $key . ",";
+        }
+
+        $str = trim($str, ",");
+        $data['id'] = $id;
+        $query = "UPDATE $this->table SET $str WHERE id = :id";
+        return $this->query($query, $data);
+    }
+
+    public function delete($id) {
+
+        $query = "DELETE FROM $this->table WHERE id = :id";
+        $data['id'] = $id;
+        return $this->query($query, $data);
+    }
+
 }
